3868b8613b77c52add1d5c87216632ab769b08bf
suryaj852 <suryaj852@gmail.com>
2025-12-12 19:34:25 +0530

    Initial commit: MyZapCart Fashion

---
+++ components/ComingSoon.js
@@ -0,0 +1,24 @@
+export default function ComingSoon() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        background: "#fdf7f3",
+        color: "#3a1515",
+        padding: "20px",
+      }}
+    >
+      <div>
+        <h1 style={{ fontSize: "2.2rem", marginBottom: "12px" }}>
+          MyZapCart Fashion
+        </h1>
+        <p style={{ fontSize: "1.1rem" }}>We are crafting something royal ✨</p>
+        <p style={{ opacity: 0.8, marginTop: "6px" }}>Launching soon.</p>
+      </div>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-// pages/_app.js
-import '../styles/globals.css';
+import "../styles/globals.css";
+import ComingSoon from "../components/ComingSoon";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  const isProd = process.env.NODE_ENV === "production";
+  const isLive = process.env.NEXT_PUBLIC_SITE_LIVE === "true";
+
+  if (isProd && !isLive) {
+    return <ComingSoon />;
+  }
+
   return <Component {...pageProps} />;
 }
